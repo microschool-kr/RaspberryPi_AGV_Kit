ea471d59a20f36c172a21aa2895c2de7d1bc19cc
AntonSangho <sanghoemail@gmail.com>
2024-07-22 17:30:34 +0900

    라인검출 될때 모터 동작까지는했으나 멈추는것은 아직 진행안함

--- src/black-line-detection.py
+++ src/black-line-detection.py
@@ -1,5 +1,22 @@
 import cv2
 import numpy as np
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+AN2 = 12
+AN1 = 13
+DIG2 = 19 
+DIG1 = 16 
+
+GPIO.setup(AN2, GPIO.OUT)
+GPIO.setup(AN1, GPIO.OUT)
+GPIO.setup(DIG2, GPIO.OUT)
+GPIO.setup(DIG1, GPIO.OUT)
+sleep(1)
+p1 = GPIO.PWM(AN1, 100)
+p2 = GPIO.PWM(AN2, 100)
 
 def detect_line(frame):
     # Convert the frame to HSV color space
@@ -43,6 +60,7 @@ def detect_line(frame):
 # Initialize the camera
 cap = cv2.VideoCapture(0)
 
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -58,7 +76,14 @@ while True:
             print("Turn Right")
         else:
             print("Go Straight")
-    
+            GPIO.output(DIG1, GPIO.HIGH)
+            GPIO.output(DIG2, GPIO.HIGH)
+            p1.start(50)
+            p2.start(50)
+            sleep(5)
+    else:
+        p1.start(0)
+        p2.start(0)
     cv2.imshow('Line Tracer', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
